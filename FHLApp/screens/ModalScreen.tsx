03c4c4584bc42f23a5df1567a95e0bedf52f4292
Syed <amanrushed@gmail.com>
2023-03-15 13:50:42 -0600

    Modified the help screen.

--- FHLApp/screens/ModalScreen.tsx
+++ FHLApp/screens/ModalScreen.tsx
@@ -5,9 +5,20 @@ import { Text, View } from '../components/Themed';
 export default function ModalScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Help</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
 
+      <View style={styles.infoContainer}>
+        <Text style={styles.infoTitle}>Festive holiday lights</Text>
+        <Text style={styles.infoText}>
+          Festive holiday lights is a fun and easy-to-use app for creating beautiful holiday light displays. With our app, you can choose from a wide range of colors and patterns to create the perfect holiday atmosphere for your home or office.
+        </Text>
+        <Text style={styles.infoTitle}>Contact us:</Text>
+        <Text style={styles.infoText}>
+          Email: support@festivelights.com
+        </Text>
+      </View>
+      
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
@@ -16,17 +27,33 @@ export default function ModalScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 0.75,
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
   },
   title: {
-    fontSize: 20,
+    fontSize: 24,
     fontWeight: 'bold',
+    marginBottom: 20,
   },
   separator: {
-    marginVertical: 30,
+    marginVertical: 20,
     height: 1,
     width: '80%',
+  },
+  infoContainer: {
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    marginVertical: 20,
+  },
+  infoTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  infoText: {
+    fontSize: 16,
+    marginBottom: 20,
   },
 });
